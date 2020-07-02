c7338c2e073bb02207752077ca0ad2d26019bb77
Merilyn Mastrangelo <64098877+MerilynMastrangelo@users.noreply.github.com>
2020-07-02 20:37:30 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <form action="./includes/textEditing.inc.php" method="post">
+            <textarea name="string" placeholder="Write some text"></textarea>
+            <input type="submit" name="reverseString-submit" value="Reverse gnirtS">
+            <input type="submit" name="uppercase-submit" value="UPPER CASE">
+            <input type="submit" name="lowercase-submit" Value="lower case">
+            <input type="submit" name="captalizecase-submit" value="Captalized case">
+        </form>
+        
+    </main>
+</body>
+</html>
